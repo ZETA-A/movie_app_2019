c05a193f54cd97d5ab624dad1c10f229aacfb7ce
SeungHyun Kim <seunghyunkim@SeungHyuns-MacBook-Pro.local>
2021-04-03 10:26:06 +0900

    필요한 파일만 두고 삭제 및 이전 내역 복구

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({favourite}) {
+  return <h1>I like {favourite}</h1>;
+}
+
+const foodILike = [
+  {
+    name: "YunUaHwel",
+    Image: "https://hoitube-image-production.s3.amazonaws.com/onul_hoi_1573643602fdf437637c.jpg"
+  },
+  {
+    name: "YaBaWiSuNDae",
+    Image: "https://lh3.googleusercontent.com/proxy/6T-m-XFF5p-m5KP9VokH26Rxva-FJj6-cvzu-85cjwmIOXpz4g4QHOpLSC0f4uNlQg4wDuZCg03vtJ1kFHFjgAM7YmOWWtY3XzBPKkjVQsoAz67rYKcyWPDaFHJ-7m4znGY0DE4yWDjFTb1n_wFDG7Bpab4L5QlvtfXGxRxvdqMMQtiGsZUFM8G0Q18NZaCKB2TjMr3pole0nsyZyJKcbNU061AxHvOWqnwia6i34V0ZZoCmUujj3tTSdI1otqxMJmPgx3hhCXgqPLlaZGvYX932rQ0gLiys3pZPzjri9I0xrQqUch8xg85IMnT1BgxAz9-DIpQl3UWDnaXPlz3E6p5se3dxajSdx8lOCUFS2tUSqwID364VZg"
+  },
+  {
+    name: ""
+  }
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
-  );
+  )
 }
-
 export default App;
